c47ec2f988e7284514b72b42349b2ac5a5337853
heroyooi <heroyooi1018@gmail.com>
2025-12-29 17:08:25 +0900

    4-3. 전역 상태 관리 도입 실습 (Pinia)

---
+++ step-04/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { useItemStore } from './stores/itemStore';
+
+const itemStore = useItemStore();
+</script>
+
+<template>
+  <div style="padding: 20px">
+    <h2>Step 4 – Pinia 전역 상태 실습</h2>
+
+    <button @click="itemStore.fetchItems">목록 불러오기</button>
+
+    <div v-if="itemStore.isLoading">로딩 중...</div>
+
+    <div v-else-if="itemStore.error">요청 실패</div>
+
+    <ul v-else>
+      <li v-for="item in itemStore.items" :key="item.id">
+        {{ item.title }}
+      </li>
+    </ul>
+  </div>
+</template>
